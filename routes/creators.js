a0b07731c31bf59ac2eda107f3e53ee11d17b7f5
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-10-18 13:33:30 -0700

    Add general commenting describing routes

--- routes/creators.js
+++ routes/creators.js
@@ -1,8 +1,7 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All routes for users creating polls are defined here
+ * Since this file is loaded in server.js into api/creators,
+ *   these routes are mounted onto /creators
  */
 
 const express = require('express');
--- routes/submittors.js
+++ routes/submittors.js
@@ -1,8 +1,7 @@
 /*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All routes for users submitting polls are defined here
+ * Since this file is loaded in server.js into api/submittors,
+ *   these routes are mounted onto /submittors
  */
 
 const express = require('express');
@@ -10,16 +9,8 @@ const { render } = require('sass');
 const router = express.Router();
 
 module.exports = (db) => {
-  // router.get("/create", (req, res) => {
-  //   res.render("create_poll");
-  // });
-
-
   router.get("/:pollId", (req, res) => {
-    // res.send(`${req.params.pollId}`);
-    // console.log("HERE", req.params.pollId);
     res.render("temp_submit_poll");
-    // res.redirect("https://www.google.com");
   });
 
   return router;
