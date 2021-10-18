975f2422144f9132cc150d19787790ca98907daa
ACristoff <acristoffanini@gmail.com>
2021-10-18 19:50:39 +0000

    outputs an object ready for processing

--- public/scripts/draggable.js
+++ public/scripts/draggable.js
@@ -1,9 +1,3 @@
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   database_url = process.env.DATABASE_URL
-// });
-
 $(document).ready(function() {
 
   const getChoices = function(id) {
@@ -23,9 +17,45 @@ $(document).ready(function() {
           var productOrder = $(this).sortable('toArray').toString();
           var productOrder2 = $(this).sortable('toArray');
           $("#sortable-9").text (productOrder);
-          console.log(productOrder2)
+          // console.log(productOrder2)
       }
     });
   });
 
+  const choicetemplate = (result) => {
+    const appendstring = `
+    <div class="draggable-rank" id ="${result.name}">${result.name}</div>
+    `;
+    return appendstring
+  }
+
+  $("form").on("submit", function (event) {
+    //prevents the default form post request, replacing it with ajax requests
+    event.preventDefault();
+
+    //empty object we will write to
+    const rankedObj = {};
+    //defines the number of choices we're ranking
+    const element = document.getElementById('sortable-8');
+    // console.log(rankings)
+    // console.log(element.childElementCount);
+
+    //appends to an object the choice and corresponding rank
+    for (i = 0; i < element.childElementCount; i++) {
+      const id = $(`.draggable-rank`).eq(i).attr("id");
+      const rank = i+1
+      rankedObj[rank] = id;
+      // console.log('id is: ', id, 'rank is: ',rank)
+    }
+
+    const nameID = 'literally me';
+    const pollId = 1;
+    const submitObj = {
+      'name': nameID,
+      'poll_id': pollId,
+      'rankings' : rankedObj
+    };
+    console.log(submitObj)
+    return submitObj;
+  });
 });
