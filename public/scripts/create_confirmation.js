96c2ca58f30096117e46b7858ca8f2130a838937
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-10-20 16:10:53 -0700

    Style create_confirmation.ejs and patch create_poll.ejs

--- public/scripts/create_confirmation.js
+++ public/scripts/create_confirmation.js
@@ -1,28 +1,55 @@
 $(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pollId = urlParams.get('pollId');
 
-  $("#submittor-link").on("click", function() {
+  $("#submittor-link").attr("href", `localhost:8080/api/submittors/poll?pollId=${pollId}`);
+  $("#admin-link").attr("href", `localhost:8080/api/creators/admin?pollId=${pollId}`);
+
+  $("#submittor-link").on("click", function(event) {
     // NO QUERY PARAM
     // const urlParams = new URLSearchParams(window.location.search);
     // const pollId = urlParams.get('pollId');
 
     // window.location.href = "/api/submittors/poll/" + pollId;
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const pollId = urlParams.get('pollId');
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const pollId = urlParams.get('pollId');
 
-    window.location.href = "/api/submittors/poll?pollId=" + pollId;
+    // window.location.href = "/api/submittors/poll?pollId=" + pollId;
+
+    event.preventDefault();
+    let copyText = $(this).attr('href');
+
+    document.addEventListener('copy', function(event) {
+      event.clipboardData.setData('text/plain', copyText);
+      event.preventDefault();
+    }, true);
+
+    document.execCommand('copy');
+    alert("Participant's Link Copied to Clipboard");
   });
 
-  $("#admin-link").on("click", function() {
+  $("#admin-link").on("click", function(event) {
     // NO QUERY PARAM
     // const urlParams = new URLSearchParams(window.location.search);
     // const pollId = urlParams.get('pollId');
 
     // window.location.href = "/api/creators/admin/" + pollId;
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const pollId = urlParams.get('pollId');
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const pollId = urlParams.get('pollId');
 
-    window.location.href = "/api/creators/admin?pollId=" + pollId;
+    // window.location.href = "/api/creators/admin?pollId=" + pollId;
+
+    event.preventDefault();
+    let copyText = $(this).attr('href');
+
+    document.addEventListener('copy', function(event) {
+      event.clipboardData.setData('text/plain', copyText);
+      event.preventDefault();
+    }, true);
+
+    document.execCommand('copy');
+    alert("Administrator's Link Copied to Clipboard");
   });
 });
