0bf1eb02ff7c8efd95f0d4b8bf2ba63599cc7284
Jordan Cronier <jcronier777@gmail.com>
2021-10-21 20:32:29 +0000

    Add error handling to bar chart

--- public/scripts/displayBarChart.js
+++ public/scripts/displayBarChart.js
@@ -1,18 +1,23 @@
 const displayBarChart = function(poll) {
+  if (poll.length === 0) {
+    displayPoll( poll[0].poll);
+    displayError();
+    return;
+  }
     const $body = $('body');
-    const barChart = $(`
+    const $barChart = $(`
         <table class="charts-css bar show-heading show-labels data-spacing-${20}" id="barchart">
-          <caption></caption>
           <tbody>
 
           </tbody>
       </table>`);
 
+      displayPoll( poll[0].poll);
+
       displayWinner(poll[0].choice);
 
-      $body.append(barChart);
+      $body.append($barChart);
       const maxPoints = findMax(poll);
-      $body.find("caption").val(poll[0].poll);
 
       for (const choice of poll) {
         $body.find("tbody").append(createRow(choice, maxPoints));
@@ -38,8 +43,16 @@ const createRow = function(choice, maxPoints) {
 };
 
 const displayWinner = function(winner) {
-  $('header').after('<h2>And the winner is...</h2>');
-  $('h2').after(`<h1>${winner}</h1>`);
+  $('h2').after('<h3>And the winner is...</h3>');
+  $('h3').after(`<h2>${winner}</h2>`);
+};
+
+const displayError = function() {
+  $('header').after('<h1>Oof! There are no results yet...<h1>');
+};
+
+const displayPoll = function(poll) {
+  $('header').after(`<h2>${poll}</h2>`);
 };
 
 $('document').ready(() => {
